72c4a3d5769d549349c935450e0342bb48de0632
Karan Singh Manral <66790855+KaranManral@users.noreply.github.com>
2023-05-02 02:32:53 +0530

    Update ai.pl

--- ai.pl
+++ ai.pl
@@ -187,8 +187,8 @@ evenoddlength:-
 Write a Prolog program to implement nth_element (N, L, X) where N is the desired position, L is a list and X represents the Nth element of L.*/
 nth_element(1, [X|_], X).
 nth_element(K,[_|L],X):-
-	nth_element(K1,L,X),
-	K is K1+1.
+    K1 is K-1,
+	nth_element(K1,L,X).
 nth_element:-
 	write("Enter the list: "),
 	read(L),
